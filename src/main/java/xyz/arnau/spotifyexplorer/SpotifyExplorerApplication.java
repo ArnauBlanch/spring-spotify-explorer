98cbe88fdce66eb0719adc079703d9d087a0f44a
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2021-03-19 14:56:45 +0100

    Use Spring Boot Security

--- src/main/java/xyz/arnau/spotifyexplorer/SpotifyExplorerApplication.java
+++ src/main/java/xyz/arnau/spotifyexplorer/SpotifyExplorerApplication.java
@@ -2,6 +2,10 @@ package xyz.arnau.spotifyexplorer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
